a7f725cbe061c14eb422b02ffca5024bed5d64e1
Lucifer <hug.m@foxmail.com>
2020-01-19 02:01:50 +0800

    qoraiche/laravel-mail-editor

---
+++ resources/views/vendor/maileclipse/templates/welcome.blade.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Skyline Welcome Email</title>
+	<style type="text/css">
+    @import url(http://fonts.googleapis.com/css?family=Lato:400);
+
+  	/* Take care of image borders and formatting */
+
+  	img {
+  		max-width: 600px;
+  		outline: none;
+  		text-decoration: none;
+  		-ms-interpolation-mode: bicubic;
+  	}
+
+    a {
+      text-decoration: none;
+      border: 0;
+      outline: none;
+    }
+
+  	a img {
+  		border: none;
+  	}
+
+    /* General styling */
+
+    td, h1, h2, h3  {
+      font-family: Helvetica, Arial, sans-serif;
+      font-weight: 400;
+    }
+
+    body {
+      -webkit-font-smoothing:antialiased;
+      -webkit-text-size-adjust:none;
+      width: 100%;
+      height: 100%;
+      color: #37302d;
+      background: #ffffff;
+    }
+
+    table {
+      background:
+    }
+
+    h1, h2, h3 {
+      padding: 0;
+      margin: 0;
+      color: #ffffff;
+      font-weight: 400;
+    }
+
+    h3 {
+      color: #21c5ba;
+      font-size: 24px;
+    }
+	</style>
+
+  <style type="text/css" media="screen">
+    @media screen {
+       /* Thanks Outlook 2013! http://goo.gl/XLxpyl*/
+      td, h1, h2, h3 {
+        font-family: 'Lato', 'Helvetica Neue', 'Arial', 'sans-serif' !important;
+      }
+    }
+  </style>
+
+  <style type="text/css" media="only screen and (max-width: 480px)">
+    /* Mobile styles */
+    @media only screen and (max-width: 480px) {
+
+      table[class="w320"] {
+        width: 320px !important;
+      }
+
+      table[class="w300"] {
+        width: 300px !important;
+      }
+
+      table[class="w290"] {
+        width: 290px !important;
+      }
+
+      td[class="w320"] {
+        width: 320px !important;
+      }
+
+      td[class="mobile-center"] {
+        text-align: center !important;
+      }
+
+      td[class="mobile-padding"] {
+        padding-left: 20px !important;
+        padding-right: 20px !important;
+        padding-bottom: 20px !important;
+      }
+    }
+  </style>
+</head>
+<body class="body" style="padding:0; margin:0; display:block; background:#ffffff; -webkit-text-size-adjust:none" bgcolor="#ffffff">
+<table width="100%" cellspacing="0" cellpadding="0" align="center">
+<tbody>
+<tr>
+<td align="center" valign="top" bgcolor="#ffffff" width="100%">
+<table width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="border-bottom: 3px solid #3bcdc3;" width="100%"><center>
+<table class="w320" width="500" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td class="mobile-center" style="padding: 10px 0; text-align: left;" valign="top"><img src="https://www.filepicker.io/api/file/TtSuN5UdTmO0NXWPfYCJ" width="250" height="62" /></td>
+</tr>
+</tbody>
+</table>
+</center></td>
+</tr>
+<tr>
+<td style="background: url('https://www.filepicker.io/api/file/zLBr1W6UT6qZP4jI2yRz') no-repeat center; background-color: #64594b; background-position: center;" valign="top" bgcolor="#64594b"><!-- [if gte mso 9]>
+          <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;height:303px;">
+            <v:fill type="tile" src="https://www.filepicker.io/api/file/ewEXNrLlTneFGtlB5ryy" color="#64594b" />
+            <v:textbox inset="0,0,0,0">
+          <![endif]-->
+<div><center>
+<table class="w320" width="530" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td class="mobile-center" style="vertical-align: middle; padding-right: 15px; padding-left: 15px; text-align: left;" valign="middle" height="303">
+<h1>WELCOME TO PACKAGE！</h1>
+<br />
+<h2>We hope you will have<br />an Awesome time!</h2>
+</td>
+</tr>
+</tbody>
+</table>
+</center></div>
+<!-- [if gte mso 9]>
+            </v:textbox>
+          </v:rect>
+          <![endif]--></td>
+</tr>
+<tr>
+<td valign="top"><center>
+<table class="w290" width="500" cellspacing="0" cellpadding="30">
+<tbody>
+<tr>
+<td style="border-bottom: 1px solid #a1a1a1;" valign="top">
+<table width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="text-align: center;">
+<h3>Free 20% off coupon code for signing-up!</h3>
+</td>
+</tr>
+</tbody>
+</table>
+<center>
+<table style="margin: 0 auto;" width="180" cellspacing="0" cellpadding="8">
+<tbody>
+<tr>
+<td style="border: 1px solid #a1a1a1; text-align: center;">Code: <span style="font-family: Courier;">4562789498</span></td>
+</tr>
+</tbody>
+</table>
+</center></td>
+</tr>
+</tbody>
+</table>
+<table class="w320" width="500" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td>
+<table width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td class="mobile-padding" style="text-align: left;"><br />We are really excited for you to join our community. Just one click away from activating your account. <br /><br /></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td class="mobile-padding">
+<table width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="width: 150px; background-color: #3bcdc3;">
+<div><!-- [if mso]>
+                            <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="#" style="height:33px;v-text-anchor:middle;width:150px;" arcsize="8%" stroke="f" fillcolor="#3bcdc3">
+                              <w:anchorlock/>
+                              <center style="color:#ffffff;font-family:sans-serif;font-size:13px;">Activate!</center>
+                            </v:roundrect>
+                          <![endif]--> <!-- [if !mso]><!-- -->
+<table width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td style="background-color: #3bcdc3; border-radius: 0px; color: #ffffff; display: inline-block; font-family: 'Lato', Helvetica, Arial, sans-serif; font-weight: bold; font-size: 13px; line-height: 33px; text-align: center; text-decoration: none; width: 150px; -webkit-text-size-adjust: none; mso-hide: all;"><span style="color: #ffffff;">Activate Now</span></td>
+</tr>
+</tbody>
+</table>
+<!--<![endif]--></div>
+</td>
+<td>&nbsp;</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td>
+<table width="100%" cellspacing="0" cellpadding="25">
+<tbody>
+<tr>
+<td>&nbsp;</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</center></td>
+</tr>
+<tr>
+<td style="background-color: #c2c2c2;"><center>
+<table class="w320" width="500" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td>
+<table width="100%" cellspacing="0" cellpadding="30">
+<tbody>
+<tr>
+<td style="text-align: center;"><a href="#"> <img src="https://www.filepicker.io/api/file/vkoOlof0QX6YCDF9cCFV" alt="twitter" width="61" height="51" /> </a> <a href="#"> <img src="https://www.filepicker.io/api/file/fZaNDx7cSPaE23OX2LbB" alt="google plus" width="61" height="51" /> </a> <a href="#"> <img src="https://www.filepicker.io/api/file/b3iHzECrTvCPEAcpRKPp" alt="facebook" width="61" height="51" /> </a></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td><center>
+<table style="margin: 0 auto;" width="100%" cellspacing="0" cellpadding="5">
+<tbody>
+<tr>
+<td style="text-align: center; margin: 0 auto;" width="100%"><a style="text-align: center;" href="#"> <img style="margin: 0 auto;" src="https://www.filepicker.io/api/file/u7CkMXcOSlG8TMbr3LnG" alt="logo link" width="123" height="24" /> </a></td>
+</tr>
+</tbody>
+</table>
+</center></td>
+</tr>
+</tbody>
+</table>
+</center></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</body>
+</html>
